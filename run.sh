ee3dbbaea1317b5a8a9074d42892498a903c42a0
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2024-11-09 19:07:19 +0100

    Add messages to run.sh

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 
 #! /bin/bash
 
-if  [[ uname == "Darwin" ]]; then
+if  [[ $(uname) == "Darwin" ]]; then
     PKG_MANAGER="brew"
-elif [[ uname == "Linux" ]]; then
+elif [[ $(uname) == "Linux" ]]; then
     PKG_MANAGER="sudo apt-get"
 else
     echo "Unrecognised OS: ${uname}. Exiting."
@@ -16,16 +16,25 @@ if ! command -v $PKG_MANAGER &> /dev/null; then
     exit 1
 fi
 
+
+echo "Running on $(uname) with ${PKG_MANAGER}"
+
+
+echo "Updating packages with ${PKG_MANAGER}"
 $PKG_MANAGER update
 $PKG_MANAGER upgrade
 
 if ! command -v git &> /dev/null; then
+    echo "Git not found. Installing."
     $PKG_MANAGER install git
 elif ! command -v gum &> /dev/null; then
+    echo "Gum not found. Installing."
     $PKG_MANAGER install gum
 elif ! command -v curl &> /dev/null; then
+    echo "curl not found. Installing."
     $PKG_MANAGER install curl
 elif ! command -v jq &> /dev/null; then
+    echo "jq not found. Installing."
     $PKG_MANAGER install jq
 fi
 
